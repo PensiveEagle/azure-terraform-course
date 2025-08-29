a82c28bb0a7e9f87bff4e959c6a1df39e3e68ab9
PensiveEagle <halljf96@gmail.com>
2025-08-29 13:49:32 +0100

    Added changes for a for each block in the ADLS

--- project/hcl/main.tf
+++ project/hcl/main.tf
@@ -3,13 +3,29 @@ resource "azurerm_resource_group" "rg-example" {
   location = var.azRegion
 }
 
+# resource "azurerm_storage_account" "mystorage" {
+#   count                    = 2
+#   name                     = "dlstestexample00${count.index}"
+#   resource_group_name      = azurerm_resource_group.rg-example.name
+#   location                 = azurerm_resource_group.rg-example.location
+#   account_tier             = "Standard"
+#   account_replication_type = "LRS"
+
+#   tags = {
+#     environment = "development"
+#   }
+# }
+
 resource "azurerm_storage_account" "mystorage" {
-  count = 2
-  name                     = "dlstestexample00${count.index}"
+  for_each = {
+    lrs = "LRS"
+    grs = "GRS"
+  }
+  name                     = "dlstestexample${each.key}00"
   resource_group_name      = azurerm_resource_group.rg-example.name
   location                 = azurerm_resource_group.rg-example.location
   account_tier             = "Standard"
-  account_replication_type = "LRS"
+  account_replication_type = each.value
 
   tags = {
     environment = "development"
