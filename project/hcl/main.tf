8aebe058a66ecefcc33c68e9f779974fc6b0912b
PensiveEagle <halljf96@gmail.com>
2025-08-29 11:25:07 +0100

    Added count to ADLS deployment
    Added a count of 2 to azurerm_storage_account deployment
    To deploy 2 storage accounts

--- project/hcl/main.tf
+++ project/hcl/main.tf
@@ -3,8 +3,9 @@ resource "azurerm_resource_group" "rg-example" {
   location = var.azRegion
 }
 
-resource "azurerm_storage_account" "dlstestexamlpe001" {
-  name                     = "dlstestexample001"
+resource "azurerm_storage_account" "mystorage" {
+  count = 2
+  name                     = "dlstestexample00${count.index}"
   resource_group_name      = azurerm_resource_group.rg-example.name
   location                 = azurerm_resource_group.rg-example.location
   account_tier             = "Standard"
